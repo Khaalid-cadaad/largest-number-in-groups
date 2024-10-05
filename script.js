6098579c42d66482767c70661c2bc4612a41a5de
unknown <khalidabdi3101@gmail.com>
2024-10-05 05:49:18 +0300

    Done

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // Good Luck 💪🏾
+function findLargestNumbers(arrays) {
+   
+    return arrays.map(subArray => Math.max(...subArray));
+}
+
+
+console.log(findLargestNumbers([[7, 3, 8, 11], [54, 12, 43, 77], [16, 11, 8]])); 
+console.log(findLargestNumbers([[22, 55, 12], [45, 66, 95], [22, 5, 11]])); 
